5055de1eb992215735c0d518e6fab3842f9897fa
Max Melcher <max@melcher.it>
2021-09-27 23:12:42 +0200

    web and state

---
+++ app/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var client = new DaprClientBuilder()
+            .Build();
+
+            var storeName = "statestore";
+            var stateKeyName = "stateKeyName";
+            var weathers = await client.GetStateAsync<WeatherForecast[]>(storeName, stateKeyName);
+
+            if (weathers == null || weathers.Length == 0)
+            {
+                var rng = new Random();
+                weathers = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+               .ToArray();
+
+                var healthy = await client.CheckHealthAsync();
+
+                await client.SaveStateAsync(storeName, stateKeyName, weathers);
+                Console.WriteLine("Saved State!");
+            }
+            return weathers;
+        }
+    }
+}
